a702fdb1d65b1f6c8dbfc65bd40fe1b04b80bed2
jpassica <jammalpassica@gmail.com>
2024-05-04 23:32:06 +0300

    Add files and folders to integrate spring application in the backend

---
+++ code/backend/my-app/src/main/java/com/mycompany/app/CrowlerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowlerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowlerSpringApplication.class, args);
+	}
+
+}
---
+++ code/backend/my-app/src/test/java/com/mycompany/app/CrowlerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowlerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
