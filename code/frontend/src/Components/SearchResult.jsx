6ca08f5e7ddf39d3773b691fff9854de57a0bf65
jpassica <jammalpassica@gmail.com>
2024-05-14 00:02:56 +0300

    Expand multithreading in phrase searching and regular searching

--- code/frontend/src/Components/SearchResult.jsx
+++ code/frontend/src/Components/SearchResult.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../Styles/style.css';
 import defaultIcon from '../Styles/img/Owl.png';
 function SearchResult({ title, link, snippet, icon }) {
-    const iconSrc = icon ? icon : defaultIcon;
+    const iconSrc = (icon && icon!="code/frontend/src/Styles/def-logo.svg") ? icon : defaultIcon;
 
     return (
         <div className='result-container'>
